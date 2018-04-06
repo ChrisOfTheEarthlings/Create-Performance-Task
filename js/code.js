07f1d2169e1cfe2bead6b9b480dd8dba64f2d67a
Chris <chriswinjum@gmail.com>
2018-04-05 23:33:31 -0700

    finished fixing canvas (hopefully forever)

--- js/code.js
+++ js/code.js
@@ -1,14 +1,36 @@
+function startGame() {
+    gameArea.start();
+    var myGamePiece = new Component(30, 30, "red", 10, 120);
+}
+
 var gameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = 1180;
-        this.canvas.height = 660;
+        this.canvas.height = 630;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[1]);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-function startGame() {
-    gameArea.start();
+function Component(width, height, color, x, y, type) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx = gameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
+//x is right, y is left, z is down
+function mapHex(x, y, z, terrain, buildingState) {
+    this.x = x;
+    this.y = y;
+    this.id = ('' + x + y + z);
+    this.terrain = terrain
+    this.buildingState = buildingState
+}
