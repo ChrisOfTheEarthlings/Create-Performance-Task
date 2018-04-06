16f0e9678058b226695fd1ffc53a72f4292bdff3
Chris <chriswinjum@gmail.com>
2018-04-05 17:34:54 -0700

    code.js now draws a line

--- js/code.js
+++ js/code.js
@@ -1,4 +1,9 @@
-var canvas = Canvas
+var canvas = document.getElementById('mainCanvas');
+var ctx = mainCanvas.getContext("2d");
+
+ctx.moveTo(0, 0);
+ctx.lineTo(1200, 600);
+ctx.stroke();
 
 console.log('hello!');
 var x = 342652356;
