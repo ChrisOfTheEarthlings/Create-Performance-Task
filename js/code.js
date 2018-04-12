fd9f2c64e2e6c4c4a70bc437e20f95fec3f58643
Chris <chriswinjum@gmail.com>
2018-04-11 23:30:39 -0700

    hex selection

--- js/code.js
+++ js/code.js
@@ -1,7 +1,7 @@
 var gameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 720;
+        this.canvas.width = 1080;
         this.canvas.height = 540;
         this.ctx = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
@@ -12,9 +12,11 @@ var gameArea = {
 }
 
 var frame = 0;
+var radius = 50;
 
 function startGame() {
     gameArea.start();
+    var interval = setInterval(update, 10);
 }
 
 //x is right, y is left, z is down
@@ -26,6 +28,7 @@ class MapHex {
         this.radius = radius;
         this.terrain = terrain || 'default';
         this.buildingState = buildingState || 'undeveloped';
+        this.selected = false;
     }
 }
 
@@ -69,6 +72,9 @@ function drawGrid(screen, grid, offset, zeroX, zeroY) {
         screen.ctx.fill();
 
         screen.ctx.fillStyle = '#608038';
+        if (hex.selected) {
+            screen.ctx.fillStyle = '#80A058';
+        }
 
         screen.ctx.beginPath();
         screen.ctx.moveTo(hexX, hexY + radius); //bottom middle
@@ -85,8 +91,13 @@ function drawGrid(screen, grid, offset, zeroX, zeroY) {
 
 function drawCursor(screen) {
     screen.ctx.fillStyle = '#000000';
-    screen.ctx.fillRect(gameArea.canvas.width / 2 - 2, gameArea.canvas.height / 2 - 6, 4, 12);
-    screen.ctx.fillRect(gameArea.canvas.width / 2 - 6, gameArea.canvas.height / 2 - 2, 12, 4);
+    screen.ctx.fillRect(centerX - 2, centerY - 6, 4, 12);
+    screen.ctx.fillRect(centerX - 6, centerY - 2, 12, 4);
+}
+
+function drawSideboard(screen) {
+    screen.ctx.fillStyle = '#404040';
+    screen.ctx.fillRect(gameArea.canvas.width * (2 / 3), 0, gameArea.canvas.width, gameArea.canvas.height);
 }
 
 function isNeighbor(grid, x1, y1, z1, x2, y2, z2) {
@@ -106,6 +117,28 @@ function isNeighbor(grid, x1, y1, z1, x2, y2, z2) {
     }
 }
 
+function selectTile(grid) {
+    var pixX = gridCenterX - centerX, pixY = gridCenterY - centerY, q, r, x, y, z;
+    
+    q = Math.round((pixX * Math.sqrt(3)/3 - pixY / 3) / (radius + 5));
+    r = Math.round(pixY * 2/3 / (radius + 5));
+
+    x = q;
+    y = -q-r;
+    z = r;
+
+    console.log('' + x + y + z)
+
+    for (i = 0; i < grid.length; i++) {
+        if ((grid[i].x === x) && (grid[i].y === y) && (grid[i].z === z)) {
+            grid[i].selected = true;
+        }
+        else {
+            grid[i].selected = false;
+        }
+    }
+}
+
 var mouseDown = false;
 gameArea.canvas.addEventListener('mousedown', function(event) { mouseDown = true; });
 window.addEventListener('mouseup', function(event) { mouseDown = false; });
@@ -115,22 +148,24 @@ gameArea.canvas.addEventListener('mousemove', function(event) {
         changeY = event.movementY;
 
     if (mouseDown) {
-        beginX += changeX;
-        beginY += changeY;
+        gridCenterX += changeX;
+        gridCenterY += changeY;
     }
 });
 
 startGame();
-var gameGrid = buildGrid(6, 6, 6, 50);
+var gameGrid = buildGrid(6, 6, 6, radius);
 
-var beginX = gameArea.canvas.width;
-var beginY = gameArea.canvas.height;
+var centerX = (gameArea.canvas.width * (2 / 3)) / 2, 
+    centerY = gameArea.canvas.height / 2,
+    gridCenterX = centerX,
+    gridCenterY = centerY;
 
 function update() {
     frame++;
     gameArea.clear();
-    drawGrid(gameArea, gameGrid, 5, beginX, beginY);
+    selectTile(gameGrid);
+    drawGrid(gameArea, gameGrid, 5, gridCenterX, gridCenterY);
     drawCursor(gameArea);
+    drawSideboard(gameArea);
 }
-
-var interval = setInterval(update, 10);
