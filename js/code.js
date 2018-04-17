b0412292920eda31afe08ca61e7b94080fba46e2
Chris <chriswinjum@gmail.com>
2018-04-17 16:51:51 -0700

    (hopefully) removed dim

--- js/code.js
+++ js/code.js
@@ -683,6 +683,7 @@ gameArea.canvas.addEventListener('touchstart', function(event) {
     var first = event.changedTouches[0]
     touchX = parseInt(first.clientX);
     touchY = parseInt(first.clientY);
+    event.preventDefault();
 });
 
 gameArea.canvas.addEventListener('touchmove', function(event) {
